297f2eff6903c34dea9788d9b757b5eb08e144d5
PritamBiswas489 <pritam.biswas489@gmail.com>
2022-10-14 12:17:09 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+app = Flask('main')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
